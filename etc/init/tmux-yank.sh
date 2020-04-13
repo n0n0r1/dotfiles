b718eb912fcde6a07cba2bd76092666933224d14
n0n0r1 <fjnr0804+n0n0r1@gmail.com>
2020-04-13 10:01:16 +0900

    fix tmux-yank.sh

--- etc/init/tmux-yank.sh
+++ etc/init/tmux-yank.sh
@@ -10,6 +10,7 @@ git clone https://github.com/tmux-plugins/tmux-yank $TMUX_YANK_PATH
 
 if [ -z "$(cat ~/.tmux.conf | grep run-shell | grep yank.tmux)" ]; then
     echo -e 'run-shell $TMUX_YANK_PATH/yank.tmux' >> ~/.tmux.conf
+    echo -e 'set -U TMUX_YANK_PATH $HOME/.tmux-yank' >> ~/.config/fish/config.fish
 fi
 
 tmux source-file ~/.tmux.conf
