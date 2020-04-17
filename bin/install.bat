99dc4e905f5cc76ee38204c7bbeb0734075dfba8
n0n0r1 <n0n0r1@outlook.jp>
2020-04-17 09:10:45 +0900

    fix install.bat

--- bin/install.bat
+++ bin/install.bat
@@ -1,15 +1,19 @@
 @echo off
 
-set DOTFILES_PATH=.
+set DOTFILES_PATH=..\
 set VSCODE_PATH=%HOMEPATH%\AppData\Roaming\Code\User
 FOR /D %%a IN (%HOMEPATH%\AppData\Local\Packages\Microsoft.WindowsTerminal_*) DO set WINDOWS_TERNAL_PATH=%%a\LocalState
 
 rem Windows Terminal profiles.json
+copy %WINDOWS_TERNAL_PATH%\profiles.json %DOTFILES_PATH%\WindowsTerminal\profiles.json.%COMPUTERNAME%
 del %WINDOWS_TERNAL_PATH%\profiles.json
-mklink %WINDOWS_TERNAL_PATH%\profiles.json %DOTFILES_PATH%\WindowsTerminal\profiles.json
+mklink %WINDOWS_TERNAL_PATH%\profiles.json %DOTFILES_PATH%\WindowsTerminal\profiles.json.%COMPUTERNAME%
 
 rem VSCode
+copy %VSCODE_PATH%\settings.json %VSCODE_PATH%\settings.json.bak
 del %VSCODE_PATH%\settings.json
 mklink %VSCODE_PATH%\settings.json %DOTFILES_PATH%\vscode\settings.json
+
+copy %VSCODE_PATH%\keybindings.json %VSCODE_PATH%\keybindings.json.bak
 del %VSCODE_PATH%\keybindings.json
 mklink %VSCODE_PATH%\keybindings.json %DOTFILES_PATH%\vscode\keybindings.json
