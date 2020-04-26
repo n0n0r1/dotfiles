a969bf47946e4d64e96ccd829b594fedecec46e3
n0n0r1 <n0n0r1@outlook.jp>
2020-04-26 20:30:51 +0900

    update tmux.sh

--- etc/init/tmux.sh
+++ etc/init/tmux.sh
@@ -2,20 +2,22 @@
 
 eval "TMUX_PATH=$TMUX_PATH"
 
-# dependencies
-sudo apt-get install -y git automake bison build-essential pkg-config libevent-dev libncurses5-dev
+if [ -z "$(command -v tmux)" ]; then
+    # dependencies
+    sudo apt-get install -y git automake bison build-essential pkg-config libevent-dev libncurses5-dev
 
-trap rm -rf $TMUX_PATH EXIT
-git clone https://github.com/tmux/tmux $TMUX_PATH
+    trap "rm -rf $TMUX_PATH" EXIT
+    git clone https://github.com/tmux/tmux $TMUX_PATH
 
-cd $TMUX_PATH
-./autogen.sh
-./configure --prefix=/usr/local
-make
-sudo make install
-tmux -V
+    cd $TMUX_PATH
+    ./autogen.sh
+    ./configure --prefix=/usr/local
+    make
+    sudo make install
+    tmux -V
+fi
 
-if [ -z "$(type wind32yank.exe > /dev/null 2>&1)" ]; then
+if [ -z "$(command -v win32yank.exe)" ]; then
     curl -LO https://github.com/equalsraf/win32yank/releases/download/v0.0.4/win32yank-x64.zip
     unzip -d $(wslpath -u "$USERPROFILE")/$WINDOWS_PATH_DIR win32yank-x64.zip
 fi
