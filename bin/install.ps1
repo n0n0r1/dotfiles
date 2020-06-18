e5c1d190f7d28e1b5e10a8457fc659181ff982bb
n0n0r1 <63493133+n0n0r1@users.noreply.github.com>
2020-06-19 02:06:24 +0900

    Update install.ps1

--- bin/install.ps1
+++ bin/install.ps1
@@ -87,11 +87,7 @@ $PIP3PACKAGES = @(
 )
 python -m pip install --upgrade $PIP3PACKAGES
 
-if (Test-Path ("$DOTFILES")) {
-  Set-Location $DOTFILES
-  git pull
-}
-else {
+if (-Not (Test-Path ("$DOTFILES"))) {
   git config --global core.autoCRLF false
   git clone https://github.com/n0n0r1/dotfiles.git $env:USERPROFILE\.dotfiles
 }
